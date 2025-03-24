dd5b6a41c9e8c655065cd67b9fd5bd10912663f1
Ravid-jam <ravid.jam02@gmail.com>
2025-03-24 19:02:04 +0530

    first commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function Page() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <div className="flex flex-col gap-44">
+        <div className="flex flex-1 flex-col items-center mt-10   gap-10">
+          <div className="flex flex-col gap-5 items-center">
+            <img src="/assets/logo.png" className="w-[68px] h-[62px]" />
+            <h1 className="text-black font-bold text-[34px]">Welcome back.</h1>
+          </div>
+          <form className="w-full flex flex-col items-center gap-7">
+            <div className="flex flex-col gap-6">
+              <input
+                placeholder="Name or Phone"
+                className="border p-3.5 border-[#DDDDDD] outline-none w-[350px] rounded-[5px] placeholder:text-[#888888] placeholder:text-[15px]"
+              />
+              <div className="flex flex-col gap-7">
+                <input
+                  type="password"
+                  placeholder="Name or Phone"
+                  className="border p-3.5 border-[#DDDDDD] outline-none w-[350px] rounded-[5px] placeholder:text-[#888888] placeholder:text-[15px]"
+                />
+                <div className="flex justify-end">
+                  <span className="text-[#888888] text-sm">
+                    Forgot password?
+                  </span>
+                </div>
+              </div>
+            </div>
+            <button className="text-white bg-gray-800 w-[350px]  font-medium rounded-lg text-base h-[60px] mx-3">
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center">
+          <div className="flex gap-1 text-center text-[#888888] text-sm">
+            Don’t have an account?&nbsp;
+            <a href="/register" className="text-black">
+              Register now
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div className="relative h-full">
+      <div className="w-full h-[50vh] z-30">
+        <img src="/assets/login.png" className="w-full" />
+      </div>
+      <div className=" flex  justify-center w-full h-full">
+        <div className="absolute -bottom-60 flex flex-col gap-20 h-full">
+          <div className="flex flex-col  items-center gap-5">
+            <img src="/assets/logo.png" className="w-[68px] h-[62px]" />
+            <div className="flex flex-col gap-4 items-center justify-center">
+              <h1 className="text-4xl max-w-xs text-center font-bold text-black">
+                Book your haircut in seconds
+              </h1>
+              <span className="text-[#888888] max-w-sm text-sm text-center">
+                Schedule your next haircut within a few seconds. Easily reserve
+                and manage your appointments.
+              </span>
+            </div>
+          </div>
+          <div className="w-full h-full flex flex-col justify-end">
+            <div className="flex flex-col gap-5 my-5">
+              <button
+                onClick={() => router.push("/login")}
+                className="text-white bg-gray-800  font-medium rounded-lg text-base h-[60px] mx-3"
+              >
+                Log In
+              </button>
+              <button className="text-black bg-none  border border-black font-medium rounded-lg text-base h-[60px] mx-3">
+                Register
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
